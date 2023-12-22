01007c56cc2e4ff2444728cb8713d2986a5b8aaf
kerimrozajac <130725852+kerimrozajac@users.noreply.github.com>
2023-12-22 09:22:48 +0100

    Update urls.py

--- django_project/urls.py
+++ django_project/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/", include("posts.urls")),  # new
     path("api-auth/", include("rest_framework.urls")),
-    path("api/v1/dj-rest-auth/", include("dj_rest_auth.urls")),
-    path("api/v1/dj-rest-auth/registration/", include("dj_rest_auth.registration.urls")),
+    path("api/v1/auth/", include("dj_rest_auth.urls")),
+    path("api/v1/auth/register/", include("dj_rest_auth.registration.urls")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/schema/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc",),
     path("api/schema/swagger-ui/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
