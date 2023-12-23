1438b2ff206be8d76543db3e12220ecdd565dc5d
kerimrozajac <130725852+kerimrozajac@users.noreply.github.com>
2023-12-23 22:56:52 +0100

    post comment only body
    updated comment serializer so that only the body is required, author and post will be inherited

--- comments/serializers.py
+++ comments/serializers.py
@@ -5,13 +5,32 @@ from accounts.models import CustomUser
 from posts.serializers import UserSerializer
 from comments.models import Comment
 from posts.models import Post
+from django.contrib.auth import get_user_model
+
+
+class CommentUserSerializer(AbstractSerializer):  # new
+
+    class Meta:
+        model = get_user_model()
+        fields = ("username", "public_id",)
+        # dodati i avatar u fields
 
 
 class CommentSerializer(AbstractSerializer):
-    author = serializers.SlugRelatedField(queryset=CustomUser.objects.all(), slug_field='public_id')
-    post = serializers.SlugRelatedField(queryset=Post.objects.all(), slug_field='public_id')
+    author = serializers.SlugRelatedField(queryset=CustomUser.objects.all(), slug_field='public_id', required=False)
+    post = serializers.SlugRelatedField(queryset=Post.objects.all(), slug_field='public_id', required=False)
 
-    """"
+    def validate(self, data):
+        # Infer the authenticated user as the author if not provided
+        # data['author'] = CustomUser.objects.get_object_by_public_id(data.get('author', self.context['request'].user))
+        data['author'] = data.get('author', self.context['request'].user)
+
+        # Infer the post from the URL if not provided
+        data['post'] = Post.objects.get_object_by_public_id(data.get('post', self.context['view'].kwargs.get('post_pk')))
+
+        return data
+
+    """
     def to_representation(self, instance):
         rep = super().to_representation(instance)
         author = CustomUser.objects.get_object_by_public_id(rep["author"])
@@ -22,7 +41,7 @@ class CommentSerializer(AbstractSerializer):
     def to_representation(self, instance):
         rep = super().to_representation(instance)
         author = CustomUser.objects.get_object_by_public_id(str(rep["author"]))  # Convert UUID to string
-        rep["author"] = UserSerializer(author).data
+        rep["author"] = CommentUserSerializer(author).data
         return rep
 
     class Meta:
@@ -30,4 +49,5 @@ class CommentSerializer(AbstractSerializer):
         # List of all the fields that can be included in a
         # request or a response
         fields = ['id', 'post', 'author', 'body', 'edited', 'created', 'updated', 'public_id']
-        read_only_fields = ["edited"]
+        # read_only_fields = ["edited"]
+        read_only_fields = ["edited", "author", "post"]
